cd8dff88383bb57aea24a65484839c8f9eef67c7
Clay Boone <tener.hades@gmail.com>
2019-10-26 14:59:43 +0100

    why use 3 lines when 1 is enough?

--- src/hiker.py
+++ src/hiker.py
@@ -2,10 +2,7 @@ from functools import reduce
 
 
 def main(input_string: str = ''):
-    vowels, words, reducer = set('aeiouy'), input_string.strip().split('/'), lambda z: reduce(lambda x, y: x + y, z, 0)
-    counts = [reducer(word[i] in vowels and word[i-1:i] not in vowels for i in range(len(word))) for word in words]
-    print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}")
-
+    list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[reduce(lambda x, y: x + y, (word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))), 0) for word in input_string.strip().split('/')]]))
 
 if __name__ == '__main__':  # pragma: no cover
     main(input('Enter your haiku: '))
