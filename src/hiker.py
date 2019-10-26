593f7f8e877e0c9383fe6f2b0ee3d34780897be1
Clay Boone <tener.hades@gmail.com>
2019-10-26 17:30:56 +0100

    waste not, want not

--- src/hiker.py
+++ src/hiker.py
@@ -1,1 +1,1 @@
-list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[sum(word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))) for word in input().strip().split('/')]]))
+list(map(lambda C: print(f"{','.join(str(c) for c in C)},{'Yes' if C == [5,7,5] else 'No'}"),[[sum(word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))) for word in input().strip().split('/')]]))
