e30c771cf3286d844bf38339173b4072fae59a41
Clay Boone <tener.hades@gmail.com>
2019-10-26 17:16:43 +0100

    goodbye functools

--- src/hiker.py
+++ src/hiker.py
@@ -1,2 +1,2 @@
-import functools, sys
-list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[functools.reduce(lambda x, y: x + y, (word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))), 0) for word in sys.stdin.readlines(1)[0].strip().split('/')]]))
+import sys
+list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[sum(word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))) for word in sys.stdin.readlines(1)[0].strip().split('/')]]))
