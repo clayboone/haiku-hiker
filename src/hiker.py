9cf097a68e94775090111a5c2392ae43965237f0
Clay Boone <tener.hades@gmail.com>
2019-10-26 15:07:45 +0100

    remove interface and interface boilerplate

--- src/hiker.py
+++ src/hiker.py
@@ -1,8 +1,2 @@
 from functools import reduce
-
-
-def main(input_string: str = ''):
-    list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[reduce(lambda x, y: x + y, (word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))), 0) for word in input_string.strip().split('/')]]))
-
-if __name__ == '__main__':  # pragma: no cover
-    main(input('Enter your haiku: '))
+list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[reduce(lambda x, y: x + y, (word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))), 0) for word in input('Enter your haiku: ').strip().split('/')]]))
