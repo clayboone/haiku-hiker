299ee2f35d43c9f036f33df40b6464beef6c426b
Clay Boone <tener.hades@gmail.com>
2019-10-26 17:22:50 +0100

    goodbye sys

--- src/hiker.py
+++ src/hiker.py
@@ -1,2 +1,1 @@
-import sys
-list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[sum(word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))) for word in sys.stdin.readlines(1)[0].strip().split('/')]]))
+list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[sum(word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))) for word in input().strip().split('/')]]))
