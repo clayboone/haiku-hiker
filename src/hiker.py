704a6e7eedcc3a25df1f4c607d3ed13175a41ea5
Clay Boone <tener.hades@gmail.com>
2019-10-26 16:45:39 +0100

    read sys.stdin instead of input()

--- src/hiker.py
+++ src/hiker.py
@@ -1,2 +1,2 @@
-from functools import reduce
-list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[reduce(lambda x, y: x + y, (word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))), 0) for word in input('Enter your haiku: ').strip().split('/')]]))
+import functools, sys
+list(map(lambda counts: print(f"{','.join(str(c) for c in counts)},{'Yes' if counts == [5, 7, 5] else 'No'}"), [[functools.reduce(lambda x, y: x + y, (word[i] in set('aeiouy') and word[i-1:i] not in set('aeiouy') for i in range(len(word))), 0) for word in sys.stdin.readlines(1)[0].strip().split('/')]]))
